58350f64a27c18a49d3b41f03c39ac033a894abc
Craig Sander <csander@centriole-solutions.com>
2016-05-11 22:17:41 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Evergreen',
+      version='1.0',
+      description='Lightweight Django CMS',
+      author='Craig Sander',
+      author_email='csander@centriole-solutions.com',
+      url='',
+      packages=['evergreen'],
+     )
