2c07273f38b32aed738752a490d29788085730df
Craig Sander <csander@centriole-solutions.com>
2016-05-26 15:21:39 -0400

    get manage index working

--- evergreen/manage/urls.py
+++ evergreen/manage/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.conf import settings
 
-from evergreen.manage import views
+from . import views
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
