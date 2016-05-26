a6fc9554ea24663079cfdeeb38640f0758c0b124
Craig Sander <csander@centriole-solutions.com>
2016-05-26 14:09:19 -0400

    FIX

--- evergreen/website/blog/urls.py
+++ evergreen/website/blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.conf import settings
 
-from evergreen.website.article import views
+from evergreen.website.blog import views
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
