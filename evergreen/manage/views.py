68206b002089e8630d6ab6c0c354c10f83f9099c
Craig Sander <csander@centriole-solutions.com>
2016-05-26 15:19:58 -0400

    begin implementing admin

--- evergreen/manage/views.py
+++ evergreen/manage/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from operator import attrgetter
 from django.contrib.auth.decorators import login_required
 
-def index(request, template_name='index.html'):
+def index(request, template_name='_index.html'):
 	
 	return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
